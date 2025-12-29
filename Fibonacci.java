c040e25585b137ac2fccfe2802463875ea1a3c10
23701A04D4 <lharithareddy13@gmail.com>
2025-12-29 16:00:36 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,14 @@
+
+class Fibonacci{
+    public static void main(String[] args){
+    int n=4;
+    int a=0;
+    int b=1;
+    for(int i=0;i<n;i++){
+        System.out.println(a+" ");
+        int c = a + b;
+        a=b;
+        b=c;
+    }
+    }
+}
