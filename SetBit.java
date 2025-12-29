793b7ee61b42f09e6666e6170f9c4b66da64ad27
23701A04D4 <lharithareddy13@gmail.com>
2025-12-29 15:59:54 +0530

    Add files via upload

---
+++ SetBit.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class SetBit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter  the n  number:");
+        int n = sc.nextInt();
+        System.out.println("enter the i number:");
+        int i = sc.nextInt();
+        int bitmask=1<<i;
+        if ((n | bitmask) != 0) {
+            System.out.println("3rd bit is 1");
+        } else {
+            System.out.println("3rd bit is 0");
+        }
+        sc.close();
+    }
+}
